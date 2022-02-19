90cda9794e6e38b301da7e5600b9233a63f346ad
vood <balitsky_igor@list.ru>
2022-02-19 15:14:34 +0300

    list

--- src/test/scala/ru/vood/generator/json/InternalJsonMeta2.scala
+++ src/test/scala/ru/vood/generator/json/InternalJsonMeta2.scala
@@ -2,15 +2,13 @@ package ru.vood.generator.json
 
 import ru.vood.generator.json.service.{JsonEntityMeta, MetaProperty}
 
-case class InternalJsonMeta(name: String) extends JsonEntityMeta[String] {
+case class InternalJsonMeta2(name: String) extends JsonEntityMeta[String] {
 
   override val entityName: String = name
 
   override val fields: Set[MetaProperty[String]] = Set(
-    strProp("type")(strConst("type_1")),
     strProp("uuid")(strConst(java.util.UUID.randomUUID().toString)),
-    numProp("operation_id")(numConst(1643184276285d)),
-    objProp("internal_object2")(InternalJsonMeta2("fghklj")),
+    numProp("operation_id")(numConst(1643184276285d))
   )
 
 }
