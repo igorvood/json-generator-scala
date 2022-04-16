124301bb8f0405d6ca84da9d14fb6e3864d32fc8
vood <balitsky_igor@list.ru>
2022-04-16 20:52:00 +0300

    proba

--- json-generator-scala/src/main/scala/ru/vood/generator/json/custom/functional/JsonEntityMetaFunction.scala
+++ json-generator-scala/src/main/scala/ru/vood/generator/json/custom/functional/JsonEntityMetaFunction.scala
@@ -1,5 +1,13 @@
 package ru.vood.generator.json.custom.functional
 
-class JsonEntityMetaFunction[ID_TYPE] extends DataTypeFunctional[ID_TYPE] {
+trait JsonEntityMetaFunction[ID_TYPE] extends DataTypeFunctional[ID_TYPE] {
+
+  val fields: Set[MetaPropertyFunctional[ID_TYPE]]
+
+  val nameEntity: String
+
+  protected val meta: MetaEntityFunctional[ID_TYPE] = MetaEntityFunctional(nameEntity, fields)
+
+  def createData[OUT_TYPE](id: ID_TYPE)(implicit convert: ID_TYPE => OUT_TYPE): OUT_TYPE = convert(id)
 
 }
